f2e2e1cab9207bfcffaa8f7cbc3cbc5bb61c8c3c
Beomi <latheledusjp@gmail.com>
2017-11-19 22:38:36 +0900

    Add Explicit Serializer model fields

--- post/serializers.py
+++ post/serializers.py
@@ -6,13 +6,16 @@ from .models import Post, Comment, Category
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
+        fields = '__all__'
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
+        fields = '__all__'
