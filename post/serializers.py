80dff7fda678cc79538a011d3307286dd6fd9056
Beomi <latheledusjp@gmail.com>
2017-11-19 22:48:37 +0900

    update comment serizalier

--- post/serializers.py
+++ post/serializers.py
@@ -3,14 +3,6 @@ from rest_framework import serializers
 from .models import Post, Comment, Category
 
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = (
-            'id', 'user','category', 'title', 'content', 'comment_set',
-        )
-
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
@@ -21,3 +13,13 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    comment_set = CommentSerializer(many=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            'id', 'user', 'category', 'title', 'content', 'comment_set',
+        )
