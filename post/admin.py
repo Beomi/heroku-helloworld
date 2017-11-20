c7c7996903dd73536fdbf969d8c489cf68328370
Beomi <latheledusjp@gmail.com>
2017-11-20 22:50:12 +0900

    Add simple admin

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Image, Comment, Category
+
+
+@admin.register(Post, Image, Comment, Category)
+class DefaultAdmin(admin.ModelAdmin):
+    pass
